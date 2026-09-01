55eecd1cf5251529bbbd45760425bdc39b5ae9e5
Edmond-1999 <agburugaedmond@gmail.com>
2026-09-01 14:33:13 +0100

    iml update

--- app/repositories/doctor_repository.py
+++ app/repositories/doctor_repository.py
@@ -1,2 +1,35 @@
+from typing import Optional, List
+from uuid import UUID
+
+
+from sqlmodel import Session, select
+
+from app.models.doctor import Doctor
+
+
 class DoctorRepository:
-    pass
+    def __init__(self, session: Session):
+        self.session = session
+
+    def save(self, doctor: Doctor):
+        self.session.add(doctor)
+        self.session.commit()
+        self.session.refresh(doctor)
+        return doctor
+
+    def find_by_id(self, doctor_id: UUID) -> Optional[Doctor]:
+        statement = select(Doctor).where(Doctor.id == doctor_id)
+        return self.session.exec(statement).first()
+
+    def find_by_email(self, email: str) -> Optional[Doctor]:
+        statement = select(Doctor).where(Doctor.email == email)
+        return self.session.exec(statement).first()
+
+    def find_all(self) -> list[Doctor]:
+        statement = select(Doctor)
+        return list(self.session.exec(statement).all())
+
+    def exists_by_email(self, email: str) -> bool:
+        return self.find_by_email(email) is not None
+
+
