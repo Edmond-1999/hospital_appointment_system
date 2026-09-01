fd198d127bb318ab4e00a8938d48ecbe58158517
KennySmart1234 <stephenkehindeolatunji@gmail.com>
2026-09-01 14:10:17 +0100

    working repository

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -1,1 +1,24 @@
 
+from datetime import datetime
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
+from app.models.appointment_status import AppointmentStatus
+
+class Appointment(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    patient_id: UUID
+    doctor_id: UUID
+    department_id: str
+    description: str
+    appointment_datetime: datetime
+    status: AppointmentStatus = AppointmentStatus.PENDING
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+
+    def confirm(self) -> None:
+        self.status = AppointmentStatus.CONFIRMED
+    def complete(self) -> None:
+        self.status = AppointmentStatus.COMPLETED
+    def cancel(self) -> None:
+        self.status = AppointmentStatus.CANCELLED
