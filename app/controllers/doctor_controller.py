9c4d20bf83ad1abc00c721df50f5bed8bc04a1bc
Edmond-1999 <agburugaedmond@gmail.com>
2026-09-02 09:07:01 +0100

    doctor service and controller update

--- app/controllers/doctor_controller.py
+++ app/controllers/doctor_controller.py
@@ -1,1 +1,33 @@
+from uuid import UUID
 
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from app.repositories.appointment_repository import AppointmentRepository
+from app.services.appointment_service import AppointmentService
+from app.services.doctor_service import DoctorService
+from app.config.dependencies import get_session
+
+
+router = APIRouter(prefix="/doctors", tags=["doctors"])
+
+
+def get_doctor_service(session: Session = Depends(get_session)):
+    appointment_repository = AppointmentRepository(session)
+    appointment_service = AppointmentService(appointment_repository)
+    return DoctorService(appointment_service)
+
+
+@router.get("/{doctor_id}/appointments")
+def view_appointments(doctor_id: UUID, service: DoctorService = Depends(get_doctor_service)):
+    return service.view_appointments(doctor_id)
+
+
+@router.get("/{doctor_id}/patients/{patient_id}")
+def view_patient(doctor_id: UUID, patient_id: UUID, service: DoctorService = Depends(get_doctor_service)):
+    return service.view_patient(patient_id)
+
+
+@router.patch("/{doctor_id}/appointments/{appointment_id}/status")
+def change_status(doctor_id: UUID, appointment_id: UUID,status: str,service: DoctorService = Depends(get_doctor_service)):
+    return service.change_status(appointment_id,status)
--- app/services/doctor_service.py
+++ app/services/doctor_service.py
@@ -4,11 +4,15 @@ from app.services.appointment_service import AppointmentService
 
 
 class DoctorService:
+
     def __init__(self, appointment_service: AppointmentService):
         self.appointment_service = appointment_service
 
     def view_appointments(self, doctor_id: UUID):
-        return self.appointment_service.get_patient_appointments(doctor_id)
+        return self.appointment_service.view_doctor_appointments(doctor_id)
 
     def view_patient(self, patient_id: UUID):
-        return self.appointment_service.get_patient(patient_id)
+        return self.appointment_service.view_patient(patient_id)
+
+    def change_status(self, appointment_id: UUID,status: str):
+        return self.appointment_service.change_status(appointment_id,status)
