7b94dfe78d7852c5a6ef393f89c111a8e535e8c0
Edmond-1999 <agburugaedmond@gmail.com>
2026-09-02 08:51:08 +0100

    update

--- app/services/doctor_service.py
+++ app/services/doctor_service.py
@@ -1,2 +1,20 @@
+from uuid import UUID
+
+from services.appointment_service import AppointmentService
+
+
 class DoctorService:
-    pass
+    def __init__(self, appointment_service: AppointmentService):
+        self.appointment_service = appointment_service
+
+    def view_appointments(self, doctor_id: UUID):
+        # return self.appointment_service.get_doctor_appointments(doctor_id)
+        pass
+
+    def view_patient(self, patient_id: UUID):
+        # return self.appointment_service.get_patient(patient_id)
+        pass
+
+    def change_status(self,appointment_id: UUID, status):
+        # return self.appointment_service.change_status(appointment_id, status)
+        pass
