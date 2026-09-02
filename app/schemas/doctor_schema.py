cdfe8afc13a34b55b4905bfc37b63864014d323a
Edmond-1999 <agburugaedmond@gmail.com>
2026-09-02 15:35:52 +0100

    doctor classes updated

--- app/schemas/doctor_schema.py
+++ app/schemas/doctor_schema.py
@@ -8,8 +8,8 @@ from app.models.user_role import UserRole
 class DoctorCreate(BaseModel):
     fullname: str
     email: EmailStr
-    password: str
     phone: str
+    password: str
     specialization: str
     department: str
 
@@ -24,10 +24,10 @@ class DoctorResponse(BaseModel):
     role: UserRole
 
 class DoctorRead(BaseModel):
-    user_id: UUID
-    fullname: str
-    email: EmailStr
-    password: str
-    phone: str
-    specialization: str
-    department: str
+    user_id = UUID
+    fullname = str
+    email = EmailStr
+    phone = str
+    password = str
+    specialization = str
+    department = str
--- app/services/doctor_service.py
+++ app/services/doctor_service.py
@@ -1,32 +1,32 @@
 from uuid import UUID
 
-from app.models.appointment_status import AppointmentStatus
-from app.models.doctor import Doctor
 from app.models.user import User
 from app.models.user_role import UserRole
-from app.repositories.doctor_repository import DoctorRepository
 from app.repositories.user_repository import UserRepository
+from app.repositories.doctor_repository import DoctorRepository
+
+from app.models.doctor import Doctor
 from app.schemas.doctor_schema import DoctorCreate, DoctorRead
 from app.services.appointment_service import AppointmentService
 
 
 class DoctorService:
 
-    def __init__(self, user_repository: UserRepository, doctor_repository: DoctorRepository, appointment_service: AppointmentService):
+    def __init__(self, appointment_service: AppointmentService, user_repository: UserRepository, doctor_repository: DoctorRepository):
+        self.appointment_service = appointment_service
         self.user_repository = user_repository
         self.doctor_repository = doctor_repository
-        self.appointment_service = appointment_service
 
     def register(self, data: DoctorCreate) -> DoctorRead:
-        if self.user_repository.find_by_email(data.email):
+        if self.user_repository.find_by_email(data.email) is not None:
             raise ValueError("Doctor already exists")
 
         user = User(
-            fullname = data.fullname,
-            email = data.email,
-            phone = data.phone,
-            password = data.password,
-            role = UserRole.DOCTOR,
+            fullname=data.fullname,
+            email=data.email,
+            phone=data.phone,
+            password=data.password,
+            role=UserRole.DOCTOR,
         )
 
         self.user_repository.create(user)
@@ -35,13 +35,12 @@ class DoctorService:
             user_id = user.id,
             specialization = data.specialization,
             department = data.department
-
         )
 
         self.doctor_repository.create(doctor)
 
         return DoctorRead(
-            user_id = doctor.user_id,
+            user_id=doctor.id,
             fullname = user.fullname,
             email = user.email,
             phone = user.phone,
@@ -50,8 +49,12 @@ class DoctorService:
             department = doctor.department
         )
 
-    def view_appointments(self, doctor_id: UUID) -> list:
-        return self.appointment_service.get_doctor_appointments(doctor_id)
 
-    def change_status(self, doctor_id: UUID, appointment_id: UUID, status: AppointmentStatus):
-        return self.appointment_service.change_status(appointment_id, doctor_id, status)
+
+
+    def view_appointments(self, doctor_id: UUID):
+        return self.appointment_service.view_doctor_appointments(doctor_id)
+
+
+    def change_status(self, appointment_id: UUID,status: str):
+        return self.appointment_service.change_status(appointment_id,status)
