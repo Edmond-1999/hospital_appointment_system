7dd49371d86b160e3a3d89f7735a89847a056db7
CodedTonye <tonyefredhorsfall101@gmail.com>
2026-09-03 10:55:38 +0100

    update

--- app/models/doctor.py
+++ app/models/doctor.py
@@ -3,6 +3,7 @@ from sqlmodel import SQLModel, Field as SQLField
 
 
 class Doctor(SQLModel, table=True):
+
     __tablename__ = "doctor"
     user_id : UUID = SQLField(foreign_key="user.id", primary_key=True)
     specialization: str
--- app/repositories/doctor_repository.py
+++ app/repositories/doctor_repository.py
@@ -17,29 +17,30 @@ class DoctorRepository:
         self.session.refresh(doctor)
         return doctor
 
-    def save(self, doctor: Doctor):
-        self.session.add(doctor)
-        self.session.commit()
-        self.session.refresh(doctor)
-        return doctor
+    # def save(self, doctor: Doctor) :
+    #     self.session.add(doctor)
+    #     self.session.commit()
+    #     self.session.refresh(doctor)
+    #     return doctor
 
     def find_by_id(self, doctor_id: UUID) -> Optional[Doctor]:
         statement = select(Doctor).where(Doctor.user_id == doctor_id)
         return self.session.exec(statement).first()
 
-    def find_by_email(self, email: str) -> Optional[Doctor]:
-        statement = select(Doctor).where(Doctor.email == email)
-        return self.session.exec(statement).first()
+    # def find_by_email(self, email: str) -> Optional[Doctor]:
+    #     statement = select(Doctor).where(Doctor.email == email)
+    #     return self.session.exec(statement).first()
 
     def find_all(self) -> list[Doctor]:
         statement = select(Doctor)
         return list(self.session.exec(statement).all())
 
-    def exists_by_email(self, email: str) -> bool:
-        return self.find_by_email(email) is not None
+    # def exists_by_email(self, email: str) -> bool:
+    #     return self.find_by_email(email) is not None
 
-    def delete(self, doctor):
-        pass
+    def delete(self, doctor: Doctor) -> None:
+        self.session.delete(doctor)
+        self.session.commit()
 
 
     def find_by_specialty(self, specialty: str) -> list[Doctor]:
